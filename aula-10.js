2528cc63a48c2e8e98602b8b04e609dbe31a5c12
Baixo <1072221661@ulife.com.br>
2024-02-26 20:50:37 -0300

    feat: aula-10

---
+++ aula-10.js
@@ -0,0 +1,11 @@
+
+
+function gasto(tempo, velocidade,) {
+    return (tempo * velocidade) / 12 ;
+}
+
+console.log('Gasto: ', gasto(10, 85).toFixed(3));
+
+console.log('Gasto: ', gasto(2, 92).toFixed(3));
+
+console.log('Gasto: ', gasto(22, 67).toFixed(3));
